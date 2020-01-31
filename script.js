0250a9596e2a0c1b747148859802410b69c9dea2
cheryl <rook2q5@gmail.com>
2020-01-31 14:57:43 -0500

    car year complete

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ form.addEventListener("input", function (event) {
       nameInput.parentElement.classList.remove("input-valid")
       nameInput.parentElement.classList.add("input-invalid")
 
-      let spellingAlert = false
+      let alert = false
       for (let child of nameInput.parentElement.childNodes) {
         if (child.textContent == "Check spelling")
-          spellingAlert = true
+          alert = true
       }
-      if (spellingAlert === false) {
+      if (alert === false) {
       let text = document.createElement("p")
       text.innerText = "Check spelling"
       nameInput.parentElement.appendChild(text)}
@@ -54,13 +54,54 @@ form.addEventListener("input", function (event) {
       console.log("Valid")
       nameInput.parentElement.classList.add("input-valid")
 
-      let spellingAlert = false
+      let alert = false
       for (let child of nameInput.parentElement.childNodes) {
         if (child.textContent == "Check spelling")
-          spellingAlert = child
+          alert = child
       } 
-      if (spellingAlert) {
-        spellingAlert.remove()
+      if (alert) {
+        alert.remove()
+      }
+    }
+  }
+})
+
+//Enter car year
+let yearInput = document.querySelector('#car-year')
+console.log(yearInput)
+
+form.addEventListener("input", function (event) {
+
+  // regular expression to match only numeric characters and spaces
+  if (yearInput.value != "") {
+
+    var re = /^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$/;
+
+    if (!re.test(yearInput.value)) {
+      console.log("Bad characters")
+      yearInput.parentElement.classList.remove("input-valid")
+      yearInput.parentElement.classList.add("input-invalid")
+
+      let alert = false
+      for (let child of yearInput.parentElement.childNodes) {
+        if (child.textContent == "Check year")
+          alert = true
+      }
+      if (alert === false) {
+      let text = document.createElement("p")
+      text.innerText = "Check year"
+      yearInput.parentElement.appendChild(text)}
+    } else {
+      console.log("Valid")
+      yearInput.parentElement.classList.add("input-valid")
+
+      let alert = false
+      for (let child of yearInput.parentElement.childNodes) {
+        if (child.textContent == "Check year")
+          alert = child
+      } 
+      if (alert) {
+        alert.remove()
       }
     }
   }
